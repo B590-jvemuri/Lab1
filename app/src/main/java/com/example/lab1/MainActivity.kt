c3b1fd2721241364fc0da9d9e5328a6afda67568
jahnavi-vemuri <jvemuri@iu.edu>
2025-01-20 11:27:04 -0500

    up to slide 19 completed

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -6,19 +6,24 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lab1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
-
+    private lateinit var binding: ActivityMainBinding
+    private val questionBank = listOf(
+        Question(R.string.question_Australia_text, true),
+        Question(R.string.question_India_text, true),
+        Question(R.string.question_USA_text, true),
+        Question(R.string.question_UK_text, false),
+        Question(R.string.question_UAE_text, false),
+        Question(R.string.question_Water_text, false)
+    )
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-        trueButton.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.trueButton.setOnClickListener {
             view: View ->
             Toast.makeText(
                 this,
@@ -26,5 +31,15 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+        binding.falseButton.setOnClickListener {
+                view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        val questionTextResId= questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
     }
 }
